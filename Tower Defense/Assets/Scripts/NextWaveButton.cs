6014778f8a401923b94dafe8334115b086150e40
Braxton <braxtonrolle00@hotmail.com>
2022-02-13 01:07:28 -0600

    Started towers, node functionality, and button disable during wave

--- Tower Defense/Assets/Scripts/NextWaveButton.cs
+++ Tower Defense/Assets/Scripts/NextWaveButton.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NextWaveButton : MonoBehaviour
 {
     public EnemySpawner es;
 
-    public void onClick()
+    void Update()
     {
         if (es.spawnFinished())
         {
-            es.findWave(es.level, es.stage);
+            gameObject.GetComponent<Button>().interactable = true;
         }
+        else
+        {
+            gameObject.GetComponent<Button>().interactable = false;
+        }
+    }
+
+    public void onClick()
+    {
+        es.findWave(es.level, es.stage);
     }
 }
--- Tower Defense/Assets/Scripts/NodeScript.cs
+++ Tower Defense/Assets/Scripts/NodeScript.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class NodeScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] GameObject baseTower;
+    private bool hasTower;
+    private void OnMouseOver()
     {
-        
+        //Check if node already has a tower present
+        if (!hasTower)
+        {
+            //Set node to dark gray on hover
+            gameObject.GetComponent<MeshRenderer>().material.color = new Color32(0x5A, 0x5A, 0x5A, 0xFF);
+            if (Input.GetMouseButtonDown(0))
+            {
+                hasTower = true;
+                Instantiate(baseTower, gameObject.transform);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseExit()
     {
-        
+        //Reset node color
+        gameObject.GetComponent<MeshRenderer>().material.color = Color.white;
     }
 }
